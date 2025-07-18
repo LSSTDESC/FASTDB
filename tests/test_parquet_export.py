68bd47e88a3e44e9eb9e2a5680a4ec3fd5b84228
Konstantin Malanchev <hombit@gmail.com>
2025-07-18 15:45:43 -0500

    Deselect UUID columns

--- tests/test_parquet_export.py
+++ tests/test_parquet_export.py
@@ -1,4 +1,4 @@
-import pandas as pd
+import nested_pandas as npd
 
 from parquet_export import dump_to_parquet
 from db import DB
@@ -8,5 +8,6 @@ def test_dump_to_parquet(alerts_90days_sent_received_and_imported, tmp_path):
     filepath = tmp_path / "test.parquet"
     with DB() as conn, open(filepath, "wb") as fp:
         dump_to_parquet(fp, procver=1, connection=conn)
-    df = pd.read_parquet(filepath, dtype_backend='pyarrow')
-    assert df.shape[0] == 181
+    nf = npd.read_parquet(filepath)
+    assert nf.shape[0] == 37
+    assert nf["diasources"].nest.flat_length == 181
