d6aae1ff6db7d46274eaa4e200fcbfcf90ed75dd
Rob Knop <raknop@lbl.gov>
2026-02-26 06:05:31 -0800

    Add more logging to soruce_importer so I can see timings

--- src/services/source_importer.py
+++ src/services/source_importer.py
@@ -2,10 +2,12 @@ import datetime
 import argparse
 import simplejson
 import textwrap
+import logging
 
 import psycopg.sql as sql
 import db
 import util
+from util import FDBLogger
 
 
 class SourceImporter:
@@ -315,6 +317,7 @@ class SourceImporter:
         Do.
         """
         with db.DBCon( dbcon ) as dbcon:
+            FDBLogger.debug( f"  ...reading mongo to temp table from {t0} to {t1}..." )
             self.read_mongo_objects( dbcon, collection, t0=t0, t1=t1, batchsize=batchsize )
 
             # Filter the temp table to just new objects
@@ -328,6 +331,7 @@ class SourceImporter:
             # Link new objects to existing root objects
             # TODO : test this with multiple processing versions and multiple
             #   objects that match!!!
+            FDBLogger.debug( "   ...linking to existing root diaobjects..." )
             dbcon.execute( "UPDATE temp_new_diaobject tno SET rootid=o.rootid\n"
                            "FROM diaobject o\n"
                            "INNER JOIN diaobject_position p ON p.diaobjectid=o.diaobjectid\n"
@@ -338,6 +342,7 @@ class SourceImporter:
                              'bpv': self.object_position_base_processing_version } )
 
             # Create new root objects
+            FDBLogger.debug( "   ...creating new root diaobjects..." )
             dbcon.execute( "CREATE TEMP TABLE temp_new_root_obj (id UUID)" )
             dbcon.execute( "INSERT INTO temp_new_root_obj "
                            "( SELECT gen_random_uuid() FROM temp_new_diaobject "
@@ -347,6 +352,7 @@ class SourceImporter:
             # created temp_new_root_obj with n rows, now just fill those n NULL rootids
             # from the n rows in temp_new_root_obj".  There must be a less byzantine
             # way to do this.
+            FDBLogger.debug( "   ...filling new rootid into temp table..." )
             dbcon.execute( "UPDATE temp_new_diaobject tno SET rootid=r.id "
                            "FROM ( ( SELECT id, ROW_NUMBER() OVER () AS n FROM temp_new_root_obj ) tnro "
                            "       INNER JOIN "
@@ -357,16 +363,21 @@ class SourceImporter:
                         "WHERE r.diaobjectid=tno.diaobjectid" )
 
             # Add the new root diaobjects
+            FDBLogger.debug( "   ...inserting new root objects into root_diaobject tables..." )
             dbcon.execute( "INSERT INTO root_diaobject ( SELECT * FROM temp_new_root_obj )" )
             nroot = dbcon.cursor.rowcount
+            FDBLogger.debug( f"      ...inserted {nroot} objects" )
 
             # Add the new objects.
+            FDBLogger.debug( "   ...inserting new diaobjects into diaobject table..." )
             dbcon.execute( "INSERT INTO diaobject(diaobjectid, rootid, base_procver_id)\n"
                            "( SELECT diaobjectid, rootid, base_procver_id FROM temp_new_diaobject )" )
             nobjs = dbcon.cursor.rowcount
+            FDBLogger.debug( f"      ...inserted {nobjs} objects" )
 
             # For diaobject position, it's simpler, we can just do an import and ignore conflicts.
 
+            FDBLogger.debug( "   ...inserting unknown positions into diaobject table..." )
             dbcon.execute( "INSERT INTO diaobject_position(diaobjectid, base_procver_id,\n"
                            "                               ra, dec, raerr, decerr, ra_dec_cov)\n"
                             "( SELECT diaobjectid, base_pos_procver_id, ra, dec, raerr, decerr, ra_dec_cov\n"
@@ -376,6 +387,7 @@ class SourceImporter:
             npos = dbcon.cursor.rowcount
 
             if commit:
+                FDBLogger.debug("   ...commiting objects" )
                 dbcon.commit()
 
             return nobjs, nroot, npos
@@ -391,22 +403,27 @@ class SourceImporter:
         with db.DBCon( dbcon ) as dbcon:
             dbcon.execute( "SET CONSTRAINTS fk_diasource_extra_diasource DEFERRED" )
 
+            FDBLogger.debug( f"   ...reading mongo to temp table from {t0} to {t1}" )
             self.read_mongo_sources( dbcon, collection, t0=t0, t1=t1, batchsize=batchsize )
             self.read_mongo_prvsources( dbcon, collection, t0=t0, t1=t1, batchsize=batchsize )
 
             nsrc = None
             nprvsrc = None
             for prv in [ "", "prv" ]:
+                FDBLogger.debug( f"   ...inserting new {prv} sources" )
                 q = ( sql.SQL( "INSERT INTO diasource( SELECT * FROM {table} ) ON CONFLICT DO NOTHING" )
                       .format( table=sql.Identifier(f"temp_{prv}diasource_import") ) )
                 dbcon.execute( q )
                 if prv == "prv":
                     nprvsrc = dbcon.cursor.rowcount
+                    FDBLogger.debug( f"      ...inserted {nprvsrc} sources from previous soruces" )
                 else:
                     nsrc = dbcon.cursor.rowcount
+                    FDBLogger.debug( f"      ...inserted {nsrc} sources" )
 
                 # For diasource extra, we want to update fields that are null, just in case some broker
                 #   gave us information that a previous broker didn't.
+                FDBLogger.debug( "   ...upserting into diasource_extra" )
                 q = ( sql.SQL( "INSERT INTO diasource_extra ( SELECT * FROM {table} )\n"
                                "ON CONFLICT (diasourceid, base_procver_id) DO UPDATE SET (\n" )
                       .format( table=sql.Identifier(f"temp_{prv}diasource_extra_import") ) )
@@ -430,8 +447,11 @@ class SourceImporter:
                 q += sql.SQL( "\n)" )
 
                 dbcon.execute( q )
+                nextra = dbcon.cursor.rowcount
+                FDBLogger.debug( f"      ...hit {nextra} rows, but I'm not 100% sure what that means" )
 
             if commit:
+                FDBLogger.debug( "   ...comitting sources" )
                 dbcon.commit()
 
             return nsrc, nprvsrc
@@ -446,15 +466,19 @@ class SourceImporter:
         with db.DBCon( dbcon ) as dbcon:
             dbcon.execute( "SET CONSTRAINTS fk_diaforcedsource_extra_diaforcedsource DEFERRED" )
 
+            FDBLogger.debug( f"   ....reading mongo to temp table from {t0} to {t1}" )
             self.read_mongo_prvforcedsources( dbcon, collection, t0=t0, t1=t1, batchsize=batchsize )
 
+            FDBLogger.debug( "   ...inserting new forcedsources" )
             dbcon.execute( "INSERT INTO diaforcedsource "
                            "( SELECT * FROM temp_prvdiaforcedsource_import ) "
                            "ON CONFLICT DO NOTHING" )
             nfrc = dbcon.cursor.rowcount
+            FDBLogger.debug( "      ...inserted {nfrc} rows" )
 
             # As with sources, for the diaforcedsource_extra table we want to update in case a
             #  broker gives us something that a previous broker didn't.
+            FDBLogger.debug( "   ...upserting into diaforcedsource_extra" )
             q = sql.SQL( "INSERT INTO diaforcedsource_extra ( SELECT * FROM temp_prvdiaforcedsource_extra_import )\n"
                           "ON CONFLICT (base_procver_id, diaobjectid, visit) DO UPDATE SET (\n" )
             first = True
@@ -476,8 +500,11 @@ class SourceImporter:
             q += sql.SQL( "\n)" )
 
             dbcon.execute( q )
+            nextra = dbcon.cursor.rowcount
+            FDBLogger.debug( f"      ...hit {nextra} rows but I'm not 100% sure what that means" )
 
             if commit:
+                FDBLogger.debug( "   ...comitting forcedsources" )
                 dbcon.commit()
 
             return nfrc
@@ -488,14 +515,18 @@ class SourceImporter:
         with db.DBCon( dbcon ) as dbcon:
             dbcon.execute( "SET CONSTRAINTS fk_diasource_brokerinfo_diasource DEFERRED" )
 
+            FDBLogger.debug( f"   ...reading mongo to temp table from {t0} to {t1}" )
             self.read_mongo_brokerinfo( dbcon, collection, t0=t0, t1=t1, batchsize=batchsize )
 
+            FDBLogger.debug( "   ...inserting new brokerinfos" )
             dbcon.execute( "INSERT INTO diasource_brokerinfo "
                            "( SELECT * FROM temp_diasource_brokerinfo_import ) "
                            "ON CONFLICT DO NOTHING" )
             ninfo = dbcon.cursor.rowcount
+            FDBLogger.debug( f"   ...inserted {ninfo} brokerinfos" )
 
             if commit:
+                FDBLogger.debug( "   ...comitting brokerinfos" )
                 dbcon.commit()
 
             return ninfo
@@ -534,9 +565,11 @@ class SourceImporter:
                                          "whenMatched": "keepExisting"
                                         } }
                           ] )
+        FDBLogger.debug( "   ...aggregating cutouts to mongo source_thumbnails collection" )
         collection.aggregate( pipeline )
 
         if commit:
+            FDBLogger.debug( "   ...committing to mongo source_thumbnails collection" )
             session.commit_transaction()
             session.end_session()
             return None
@@ -577,49 +610,65 @@ class SourceImporter:
 
         """
 
-        # Everything happens in one transaction, until the commit() at the end
-        #   of this block.  Make sure that none of the functions called
-        #   end the transaction in dbcon.
-        with db.DBCon() as dbcon:
-            timestampexists = False
-            rows, _cols = dbcon.execute( "SELECT t FROM diasource_import_time WHERE collection=%(col)s",
-                                         { 'col': collection.name } )
-            if len(rows) == 0:
-                t0 = datetime.datetime( 1970, 1, 1, 0, 0, 0, tzinfo=datetime.UTC )
-            else:
-                timestampexists = True
-                t0 = rows[0][0]
+        try:
+            # Everything happens in one transaction, until the commit() at the end
+            #   of this block.  Make sure that none of the functions called
+            #   end the transaction in dbcon.
+            with db.DBCon() as dbcon:
+                timestampexists = False
+                rows, _cols = dbcon.execute( "SELECT t FROM diasource_import_time WHERE collection=%(col)s",
+                                             { 'col': collection.name } )
+                if len(rows) == 0:
+                    t0 = datetime.datetime( 1970, 1, 1, 0, 0, 0, tzinfo=datetime.UTC )
+                else:
+                    timestampexists = True
+                    t0 = rows[0][0]
 
-            t1 = datetime.datetime.now( tz=datetime.UTC )
+                t1 = datetime.datetime.now( tz=datetime.UTC )
 
-            # Make sure foreign key constraints aren't goign to trip us up
-            #   below, but that they're only checked at the end of the transaction.
-            dbcon.execute( "SET CONSTRAINTS fk_diasource_diaobject DEFERRED" )
-            dbcon.execute( "SET CONSTRAINTS fk_diaforcedsource_diaobject DEFERRED" )
+                # Make sure foreign key constraints aren't goign to trip us up
+                #   below, but that they're only checked at the end of the transaction.
+                dbcon.execute( "SET CONSTRAINTS fk_diasource_diaobject DEFERRED" )
+                dbcon.execute( "SET CONSTRAINTS fk_diaforcedsource_diaobject DEFERRED" )
 
-            nobj, nroot, npos = self.import_objects_from_collection( collection, t0, t1, dbcon=dbcon, commit=False )
-            nsrc, nprvsrc = self.import_sources_from_collection( collection, t0, t1, dbcon=dbcon, commit=False )
-            nfrc = self.import_forcedsources_from_collection( collection, t0, t1, dbcon=dbcon, commit=False )
-            ninfo = self.import_brokerinfo_from_collection( collection, t0, t1, dbcon=dbcon, commit=False )
-            mongosession = self.import_cutouts_from_collection( collection, t0, t1, commit=False )
+                FDBLogger.debug( "Improting objects..." )
+                nobj, nroot, npos = self.import_objects_from_collection( collection, t0, t1, dbcon=dbcon, commit=False )
+                FDBLogger.debug( "Improting sources..." )
+                nsrc, nprvsrc = self.import_sources_from_collection( collection, t0, t1, dbcon=dbcon, commit=False )
+                FDBLogger.debug( "Improting forcedsources..." )
+                nfrc = self.import_forcedsources_from_collection( collection, t0, t1, dbcon=dbcon, commit=False )
+                FDBLogger.debug( "Improting brokerinfos..." )
+                ninfo = self.import_brokerinfo_from_collection( collection, t0, t1, dbcon=dbcon, commit=False )
+                FDBLogger.debug( "Improting ocutouts..." )
+                mongosession = self.import_cutouts_from_collection( collection, t0, t1, commit=False )
 
-            if timestampexists:
-                dbcon.execute( "UPDATE diasource_import_time SET t=%(t)s WHERE collection=%(col)s",
-                               { 't': t1, 'col': collection.name } )
-            else:
-                dbcon.execute( "INSERT INTO diasource_import_time(collection,t) "
-                               "VALUES(%(col)s,%(t)s)",
-                               { 't': t1, 'col': collection.name } )
+                FDBLogger.debug( "Updating diasource_import_time..." )
+                if timestampexists:
+                    dbcon.execute( "UPDATE diasource_import_time SET t=%(t)s WHERE collection=%(col)s",
+                                   { 't': t1, 'col': collection.name } )
+                else:
+                    dbcon.execute( "INSERT INTO diasource_import_time(collection,t) "
+                                   "VALUES(%(col)s,%(t)s)",
+                                   { 't': t1, 'col': collection.name } )
 
-            # Only commit once at the end.  That way, if anything goes wrong,
-            #   the database will be rolled back.  No objects or sources will
-            #   have been saved, and the timestamp will not have been updated.
-            # The timestamp will be updated if and only if everything imported.
-            mongosession.commit_transaction()
-            mongosession.end_session()
-            dbcon.commit()
+                # Only commit once at the end.  That way, if anything goes wrong,
+                #   the database will be rolled back.  No objects or sources will
+                #   have been saved, and the timestamp will not have been updated.
+                # The timestamp will be updated if and only if everything imported.
+                FDBLogger.debug( "Committing mongo..." )
+                mongosession.commit_transaction()
+                mongosession.end_session()
+                FDBLogger.debug( "Committing postgres..." )
+                dbcon.commit()
 
-        return nobj, nroot, npos, nsrc, nprvsrc, nfrc, ninfo
+                FDBLogger.debug( "Done." )
+
+            return nobj, nroot, npos, nsrc, nprvsrc, nfrc, ninfo
+        except Exception:
+            # This is just so we get the timestamp in the log
+            FDBLogger.error( "Exception." )
+            raise
+
 
 
 # ======================================================================
@@ -627,6 +676,8 @@ class SourceImporter:
 def main():
     parser = argparse.ArgumentParser( 'source_importer.py', description='Import sources from mongo to postgres',
                                       formatter_class=argparse.ArgumentDefaultsHelpFormatter )
+    parser.add_argument( "-c", "--collection", required=True, nargs='+',
+                         help="MongoDB collections to import from" )
     parser.add_argument( "-o", "--object-base-processing-version", default=None,
                          help="Base processing version (uuid or text) to tag imported objects with." )
     parser.add_argument( "-p", "--object-position-base-processing-version", default=None,
@@ -638,9 +689,14 @@ def main():
     parser.add_argument( "-H", "--host-base-processing-version", default=None,
                          help=( "Base processing verson (uuid or text) to tag imported hosts with.  "
                                 "Not currently used." ) )
-    parser.add_argument( "-c", "--collection", required=True, nargs='+',
-                         help="MongoDB collections to import from" )
+    parser.add_argument( "-v", "--verbose", action='store_true', default=False,
+                         help="Show debug log messages" )
     args = parser.parse_args()
+
+    if args.verbose:
+        FDBLogger.setLevel( logging.DEBUG )
+    else:
+        FDBLogger.setLevel( logging.INFO )
 
     si = SourceImporter( args.object_base_processing_version,
                          args.object_position_base_processing_version,
@@ -658,14 +714,14 @@ def main():
     with db.MG() as mg:
         for collection_name in args.collection:
 
-            util.FDBLogger.info( f"Importing from {collection_name}..." )
+            FDBLogger.info( f"Importing from {collection_name}..." )
 
             collection = db.get_mongo_collection( mg, collection_name )
             nobj, nroot, npos, nsrc, nprvsrc, nfrc, ninfo = si.import_from_mongo( collection )
 
-            util.FDBLogger.info( f"...imported {nobj} objects, {nroot} root objects, {npos} object positions, "
-                                 f"{nsrc+nprvsrc} sources ({nsrc} main, {nprvsrc} previous), {nfrc} forced sources, "
-                                 f"{ninfo} broker infos from {collection_name}" )
+            FDBLogger.info( f"...imported {nobj} objects, {nroot} root objects, {npos} object positions, "
+                            f"{nsrc+nprvsrc} sources ({nsrc} main, {nprvsrc} previous), {nfrc} forced sources, "
+                            f"{ninfo} broker infos from {collection_name}" )
             totnobj += nobj
             totnroot += nroot
             totnpos += npos
@@ -674,9 +730,9 @@ def main():
             totnfrc += nfrc
             totninfo += ninfo
 
-    util.FDBLogger.info( f"Overall, imported {totnobj} objects, {totnroot} root objects, {totnpos} object positions, "
-                         f"{totnsrc+totnprvsrc} sources ({totnsrc} main, {totnprvsrc} previous), "
-                         f"{totnfrc} forced sources, {totninfo} broker infos from {collection_name}" )
+    FDBLogger.info( f"Overall, imported {totnobj} objects, {totnroot} root objects, {totnpos} object positions, "
+                    f"{totnsrc+totnprvsrc} sources ({totnsrc} main, {totnprvsrc} previous), "
+                    f"{totnfrc} forced sources, {totninfo} broker infos from {collection_name}" )
 
 
 # ======================================================================
