79221126f07fa1ce70b237dbee19c505abc5000f
Rob Knop <rknop@pobox.com>
2026-04-29 06:31:06 -0700

    Mark db/test_ppdb.py with a skipif since it was breaking with a session fixture other tests loaded; finally fix the dbapp/test_sql_queries.py for changes that were made to fixtures a long time ago.

--- tests/dbapp/test_sql_queries.py
+++ tests/dbapp/test_sql_queries.py
@@ -1,28 +1,39 @@
+import pytest
 import sys
 import io
 import pandas
+import itertools
 
 sys.path.insert( 0, '/code/client' )
 from fastdb_client import FASTDBClient
 
 
-def test_short_query( obj1, src1, src1_pv2, test_user ):
+@pytest.fixture
+def test_sql_query_expecteddata( set_of_lightcurves ):
+    expecteddata = set(
+        itertools.chain(
+            *[ itertools.chain(
+                *[ itertools.chain( *[ [ ( s.diasourceid, s.diaobjectid, s.visit, str(s.base_procver_id) )
+                                         for s in slist ] ] )
+                   for slist in rstruct['src'].values() ] )
+               for rstruct in set_of_lightcurves ] ) )
+
+    return expecteddata
+
+
+def test_short_query( test_user, test_sql_query_expecteddata ):
     fastdb = FASTDBClient( 'http://webap:8080', username='test', password='test_password' )
 
     res = fastdb.submit_short_sql_query( "SELECT * FROM diasource" )
-    assert len(res) == 2
-    assert set( [ r['diaobjectid'] for r in res ] ) == { src1.diaobjectid, src1_pv2.diaobjectid }
-    assert set( [ r['visit'] for r in res ] ) == { src1.visit, src1_pv2.visit }
-    assert set( [ r['base_procver_id'] for r in res ] ) == { str(src1.base_procver_id), str(src1_pv2.base_procver_id) }
+    founddata = set( ( r['diasourceid'], r['diaobjectid'], r['visit'], r['base_procver_id'] ) for r in res )
+    assert founddata == test_sql_query_expecteddata
 
 
-def test_synchronous_long_query( obj1, src1, src1_pv2, test_user ):
+def test_synchronous_long_query( test_user, test_sql_query_expecteddata ):
     fastdb = FASTDBClient( 'http://webap:8080', username='test', password='test_password' )
 
     res = fastdb.synchronous_long_sql_query( "SELECT * FROM diasource", checkeach=1, maxwait=20 )
     strio = io.StringIO( res )
     df = pandas.read_csv( strio, sep=',', header=0 )
-    assert len(df) == 2
-    assert all( df.diaobjectid.values == [42,42] )
-    assert all( df.visit.values == [64,64] )
-    assert set( df.base_procver_id.values ) == { str(src1.base_procver_id), str(src1_pv2.base_procver_id) }
+    founddata = set( ( r.diasourceid, r.diaobjectid, r.visit, r.base_procver_id ) for r in df.itertuples() )
+    assert founddata == test_sql_query_expecteddata
