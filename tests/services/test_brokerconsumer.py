40380b859fea7f435a4643ce61c54d6b5ae5848d
Rob Knop <raknop@lbl.gov>
2026-07-24 14:36:08 -0700

    Modifications to pitt-google brokerconsumer to ingest a science pipeline.  (Not tested yet.)  Modify docker file to stop using a pubsub patch and now install a fork I made.  Also replace pypi version pins with ranges.

--- tests/services/test_brokerconsumer.py
+++ tests/services/test_brokerconsumer.py
@@ -555,6 +555,57 @@ def test_pittgoogle():
 
 
 @pytest.mark.skipif( not env_as_bool('RUN_PITTGOOGLE_TESTS'), reason='RUN_PITTGOOGLE_TESTS is not set' )
+def test_pittgoogle_scipipe():
+    barf = "".join( random.choices( 'abcdefghijklmnopqrstuvwxyz', k=6 ) )
+    brokertopic = 'supernnova'
+    groupid = f'fastdb-test-{barf}'
+    os.environ['GOOGLE_CLOUD_PROJECT'] = 'fastdb-test-20251103'
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/secrets/fastdb-test-20251103-5a0f5182da01.json'
+
+    expectedcollections = [ f'fastdb_test_pittgoogle_supernnova_{s}' for s in
+                            [ 'diaobject', 'diasource', 'diasource_extra',
+                              'diaforcedsource', 'diaforcedsource_extra',
+                              'thumbnails', 'brokerinfo', 'alertcache' ] ]
+
+    try:
+        t0 = time.perf_counter()
+        pgb = PittGoogleConsumer( groupid=groupid, max_workers=2, batch_size=10, consume_timeout=2,
+                                  survey='lsst', topic_name=brokertopic, cache_alerts=True,
+                                  schemafile=None, alert_format='json',
+                                  mongodb_collection_base='fastdb_test_pittgoogle_supernnova' )
+        FDBLogger.info( "Running PittGoogleBroker.poll() for 10s...." )
+        pgb.poll( restart_time=datetime.timedelta( seconds=10 ), max_restarts=1 )
+        dt = time.perf_counter() - t0
+        FDBLogger.info( f"Returned from PittGoogleBroker.poll(), it handled {pgb.tot_n_messages_consumed} messages.  "
+                        f"Creation plus poll time: {dt:.2f} sec." )
+
+        with db.MGCon() as mg:
+            assert all( i in mg.db.list_collection_names() for i in expectedcollections )
+            nalerts = mg.collection( 'fastdb_test_pittgoogle_supernnova_alertcache' ).count_documents({})
+            assert nalerts == pgb.tot_n_messages_consumed
+            assert nalerts > 3
+            col = mg.collection( 'fastdb_test_pittgoogle_supernnova_brokerinfo' )
+            assert col.count_documents({}) == nalerts
+            srcids = set()
+            for doc in col.find( {} ):
+                assert doc['brokername'] == 'Pitt-Google'
+                assert doc['topic'] == f'lsst-{brokertopic}'
+                srcids.add( doc['diasourceid'] )
+
+            col = mg.collection( 'fastdb_test_pittgoogle_supernnova_diasource' )
+            assert srcids.issubset( set( c['diasourceid'] for c in col.find({}) ) )
+
+            # Check other stuff?
+
+    finally:
+        with db.MGCon() as mg:
+            for col in expectedcollections:
+                mg.collection( col ).drop()
+
+
+
+
+@pytest.mark.skipif( not env_as_bool('RUN_PITTGOOGLE_TESTS'), reason='RUN_PITTGOOGLE_TESTS is not set' )
 def test_pittgoogle_launcher():
     os.environ['GOOGLE_CLOUD_PROJECT'] = 'fastdb-test-20251103'
     os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/secrets/fastdb-test-20251103-5a0f5182da01.json'
